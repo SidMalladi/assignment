c4a3dfc393e4c37e2d4788425fa78afa8d19fb0f
Siddharth Malladi <39798296+SidMalladi@users.noreply.github.com>
2021-08-31 17:18:51 +0530

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  assignment
+//
+//  Created by Siddharth Malladi on 12/08/21.
+//  Copyright © 2021 Siddharth Malladi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.orange.frame(width: 300, height: 700).clipped()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
